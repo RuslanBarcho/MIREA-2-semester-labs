281d45d24a7873090106090725ef438a3f2c3e1c
Ruslan <RRCFoxyLoxy@yandex.ru>
2018-02-19 14:16:50 +0300

    update

--- task_1.cpp
+++ task_1.cpp
@@ -7,6 +7,12 @@ vector <int> arr;
 void showArray(std::vector<int> array){
   for(int i = 0; i < array.size(); i++) cout << array[i] << ' ';
 }
+void showArrayCType(std::vector<int> array){
+  for(int i = 0; i < array.size(); i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
 //cyclic shift
 void move(int value, vector<int> toMove){
   rotate(toMove.rbegin() , toMove.rbegin() + value , toMove.rend()) ;
@@ -19,12 +25,10 @@ int main() {
   srand(time(NULL));
   //auto-input
   for (int i=0; i<q; i++){
-    int q; std::cin >> q;
-    arr.push_back(q);
-    //arr.push_back(10 + rand() % (50 - 10 + 1));
+    arr.push_back(10 + rand() % (50 - 10 + 1));
   }
   showArray(arr);
-  cout << "enter value to move array" << '\n';
+  cout << '\n' << "enter value to move array" << '\n';
   int g; cin>>g;
   move(g, arr);
   return 0;
